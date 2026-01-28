b6150f53ce18b145f329c9a29e3daa469ba32b34
whsage <whsage@qq.com>
2026-01-28 22:55:02 +0800

    fix: 添加react-helmet-async依赖和HelmetProvider，修复数学辅导页面无法访问的问题

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { UserProvider } from './context/UserContext'
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <HelmetProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </HelmetProvider>
   </StrictMode>,
 )
+
